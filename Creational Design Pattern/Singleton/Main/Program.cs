adcce24f54ec8b1721927de11c0e2a99e6ac6591
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-04-19 21:49:30 +0530

    adding Singletoncls

---
+++ Creational Design Pattern/Singleton/Main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SinglenonLib.singletonNormal;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singletoncls fromTeachaer =  Singletoncls.GetSingleton;
+            fromTeachaer.InstanceMessage("From Tacher");
+            
+            Singletoncls fromStudent =  Singletoncls.GetSingleton;
+            fromStudent.InstanceMessage("From Student");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Creational Design Pattern/Singleton/SinglenonLib/singletonNormal/Singletoncls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace SinglenonLib.singletonNormal
+{
+    public class Singletoncls
+    {
+        public static int counter=0;
+        public static Singletoncls instance=null;
+        public static Singletoncls GetSingleton {  // meka field ekak 
+            get {
+                    if(instance==null){
+                        instance=new Singletoncls();
+                    }
+
+                    else{
+                        return instance;
+                    }
+                    return instance;
+            }
+        }
+
+        private Singletoncls() // console kiyana eka ganne using system kiyana eken
+
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());  
+        }
+
+        public  void InstanceMessage(string st){
+            Console.WriteLine(st);
+        }
+
+    }
+}
